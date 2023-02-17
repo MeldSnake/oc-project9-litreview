bb3d20b8bf22ea0cbd948313c86c68920c81518d
MeldSnake <ikeek.33@gmail.com>
2023-02-17 21:01:59 +0100

    Updated UI
    -Ticket template form replacement.
    -Updated some styles.
    -HTML W3C fixes.
    
    Signed-off-by: MeldSnake <ikeek.33@gmail.com>

--- listings/widgets.py
+++ listings/widgets.py
@@ -1,12 +1,6 @@
 from django import forms
-from django.utils.safestring import mark_safe
 
 
 class TicketImagePreviewWidget(forms.widgets.ClearableFileInput):
 
-    def render(self, name: str, value, attrs=None, renderer=None, **kwargs):
-        origin = super().render(name, value, attrs, renderer, **kwargs)
-        if value:
-            preview = mark_safe(f"<picture class='ticket__cover'><img class='ticket__cover__image' src='{value.url}' alt='Cover'/></picture>")
-            return f"{preview}{origin}"
-        return origin
+    template_name = "image_form.html"
