a3dbb6a000885ce93e21c27b2b0f0c8e15988161
MeldSnake <ikeek.33@gmail.com>
2023-02-19 20:31:44 +0100

    Removed unused fields for user.
    Signed-off-by: MeldSnake <ikeek.33@gmail.com>

--- credentials/forms.py
+++ credentials/forms.py
@@ -1,15 +1,13 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import UsernameField
-from django.forms import EmailField
 
 
 class RegisterForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ("username", "email", "first_name", "last_name")
+        fields = ("username",)
         field_classes = {
             'username': UsernameField,
-            'email': EmailField,
         }
